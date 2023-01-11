4c43f135f6192cd3afc95714dc2904a95f4a0339
eyeofsol <kingjihong@naver.com>
2023-01-11 11:26:24 +0900

    fix: fetch타입 변경

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -19,5 +19,16 @@ public class Member {
     private Address address;
 
     @OneToMany(mappedBy = "member") // 주인이 아니기 때문
+    // 컬렉션은 필드에서 바로 초기화하는게 좋음!!!
     private List<Order> orders = new ArrayList<>();
+
+    /*
+    public Member() {
+        orders = new ArrayList<>();
+    }
+    이런 식으로 하면 널을 고려해야 함.
+    그리고 가장 중요한 건 하이버네이트는 엔티티를 영속화 할 때, 컬렉션을 하이버네이트 내장 컬렉션으로 전환(감싼다)
+    따라서 임의접근 시 오류가 발생할 수 있음.
+    만들고 건들지말자.
+     */
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,18 +17,55 @@ public class Order {
     @Column(name = "order_id") // 테이블_아이디로 맞추는게 좋음
     private Long id;
 
-    @ManyToOne // 양방향이라 연관관게 주인 정해줘야 함.
+    // to many 시리즈는 기본이 레이지라 괜찮은데 투 원는 무조건 페치타입 레이지로 다 바꿔줘야 함.
+    @ManyToOne(fetch = FetchType.LAZY) // 양방향이라 연관관게 주인 정해줘야 함.
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne // 오더에 포린키를 주자. 접근이 더 많이 일어나므로
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // 오더에 포린키를 주자. 접근이 더 많이 일어나므로
     @JoinColumn(name = "delivery")
     private Delivery delivery;
 
     private LocalDateTime orderTime; // 주문시간
 
+    @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCLE]
+
+    //==연관관계 메서드==//
+    /*
+    ex) 다음과 같다.
+    psvm() {
+        Member member = new Member();
+        Order order = new Order();
+
+        member.getOrders().add(order);
+        order.setMember(member);
+    }
+     */
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    /*
+    ex) 위 코드를 작성한다면 아래와 같아짐 편의성 증가
+    psvm() {
+        Member member = new Member();
+        Order order = new Order();
+
+        order.setMember(member);
+    }
+     */
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
